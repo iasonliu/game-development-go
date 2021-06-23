ac4547ab478b75d44d714bf248ee0cc91815e15c
Xiangxu Liu <xxliu@toughtworks.com>
2021-06-23 16:01:12 +0800

    add rand seed

--- shooter-game/cmd/shooter/main.go
+++ shooter-game/cmd/shooter/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/iasonliu/game-development-go/shooter-game"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	game := shooter.NewGame()
 	if err := game.Run(); err != nil {
 		log.Fatalf("Game error: %v", err)
